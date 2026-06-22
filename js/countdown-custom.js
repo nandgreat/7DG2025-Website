ad5d0a97db1df684b82e0df53e2e462678df4373
Nandom <paul4nank@gmail.com>
2026-06-22 12:38:56 +0100

    updated website

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2025, 6, 1, 12)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2026, 6, 1, 6)}); // year, month, date, hour
         });
 });		
 
